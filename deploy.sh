289fbcd0dc2fd52c9f11dab4cc6441bb6d42b0c9
Kerin Cosford <cosford@gmail.com>
2016-05-05 11:27:30 +0100

    Updated deploy script to use terraform remote config

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 set -e
 
-# exit if no ami_id or config version specified
-[[ $# = 2 ]] || { echo "Usage: $0 AMI_ID CONFIG_VERSION"; exit 1; }
+# exit if no env, ami_id or config version specified
+[[ $# = 3 ]] || { echo "Usage: $0 ENV AMI_ID CONFIG_VERSION"; exit 1; }
+
+ENV=$1
+AMI_ID=$2
+CONFIG_VERSION=$3
 
 cd ../csd-notes-infrastructure
 
-terraform refresh
+terraform remote config -backend=s3 -backend-config="bucket=csd-notes-terraform"\
+  -backend-config="key=${ENV}.tfstate" -backend-config="region=eu-west-1"
+
+terraform remote pull
 
 echo "
 Deploying with args:
-image_id = $1
-config_version = $2
+image_id = ${AMI_ID}
+config_version = ${CONFIG_VERSION}
 env = $(terraform output environment)
 lc_security_groups = $(terraform output web_sg_id)
 load_balancers = $(terraform output elb_name)
@@ -20,8 +27,8 @@ asg_subnets = $(terraform output private_subnets)
 "
 
 ansible-playbook ../csd-notes-deploy/ansible/rolling_ami.yml -vv --extra-vars \
-"image_id=$1
-config_version=$2
+"image_id=${AMI_ID}
+config_version=${CONFIG_VERSION}
 env=$(terraform output environment)
 lc_security_groups=$(terraform output web_sg_id)
 load_balancers=$(terraform output elb_name)
